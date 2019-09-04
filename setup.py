8db36b2bbd7d3ba8fd693087feed6976c521d546
Adam Frank <afrank@mozilla.com>
2019-09-04 11:13:54 -0700

    adding click dependency for tests

--- setup.py
+++ setup.py
@@ -17,5 +17,6 @@ setup(name='fxa-ingest',
         'grpcio-gcp==0.2.2',   # this seems to be explicitly required on debian python 3.5  ?!
         'user-agents==2.0',
         'boto3==1.9.159',
+        'click',
       ]
     )
